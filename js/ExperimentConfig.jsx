8a8104270275d5b2aac1906bf4adc0776cf117b9
Mikel Eguiraun <Mikel.Eguiraun@maxlab.lu.se>
2015-10-08 11:03:39 +0200

    link experiment config with the queue (backbone events) add new elemements to the queue (+remove)

--- js/ExperimentConfig.jsx
+++ js/ExperimentConfig.jsx
@@ -1,15 +1,37 @@
 /** @jsx React.DOM */
-
 var ExperimentConfiguration = React.createClass({
+  addCentring: function(){
+    window.app_dispatcher.trigger("queue:new_item", 
+      { kind: "SampleCentring", 
+        text: "params",
+      });
+  },
+  addCharacterisation: function(){
+    window.app_dispatcher.trigger("queue:new_item", 
+      { kind: "Characterisation", 
+        text: "params",
+      });
+  },
+  addStandardCollection: function(){
+    window.app_dispatcher.trigger("queue:new_item", 
+      { kind: "StandardCollection", 
+        text: "params",
+      });
+    console.log('event sent...')
+  },
+  addHelicalCollection: function(){
+  },
+  addMeshCollection: function(){
+  },
   render: function(){
     return (<div className="panel panel-info">
                       <div className="panel-heading">
                         <h3 className="panel-title">Experiment Configuration</h3>
                       </div>
                       <div className="panel body">
-                        <p>Add collection method</p>
-                        <a className="btn btn-primary"><i className="fa fa-fw fa-plus-square"></i>Characterisation</a>
-                        <a className="btn btn-primary"><i className="fa fa-fw fa-plus-square"></i>Standard Collection</a>
+                        <a className="btn btn-primary" onClick={this.addCentring}><i className="fa fa-fw fa-plus-square"></i>Centring</a>
+                        <a className="btn btn-primary" onClick={this.addCharacterisation}><i className="fa fa-fw fa-plus-square"></i>Characterisation</a>
+                        <a className="btn btn-primary" onClick={this.addStandardCollection}><i className="fa fa-fw fa-plus-square"></i>Standard Collection</a>
                         <div className="btn-group">
                           <a className="btn btn-primary dropdown-toggle" data-toggle="dropdown"><span className="fa fa-caret-down"></span> Advanced </a>
                           <ul className="dropdown-menu" role="menu">
--- js/SampleTree.jsx
+++ js/SampleTree.jsx
@@ -17,14 +17,35 @@ var params = { Characterisation : {
 var SingleSampleTree = React.createClass({
 
 	getInitialState: function () {
+      ///status: 0 not done, 1 done, 2 failed
       return {
-          list: ['Characterisation', 'SampleCentring', 'StandardCollection'],
+          list: [{name: 'SampleCentring_1', status: 0, params:{}}, {name: 'Characterisation_1', status: 1, params:{}}, {name : 'SampleCentring_1', status: 0, params:{}}, {name: 'StandardCollection_1', status: 2, params:{}}],
           sampleName: 'Sample_42'  
       }
     },
 
+  addQueueItem: function(newItem){
+    var auxList = this.state.list
+    auxList.push({name :this.generateElementId(newItem['kind']), status:0, params:{}})
+    this.setState({list: auxList})
+  },
+  removeQueueItem: function(itemToRemove){
+    console.log(itemToRemove)
+    var auxList = this.state.list
+    var index = auxList.indexOf(itemToRemove)
+    auxList.splice(index, 1)
+    this.setState({list: auxList})
+  },
+  runThisItem: function(item){
+    console.log(item)
+
+  },
   componentWillMount: function(){
-      this.getInitialState()    
+    window.app_dispatcher.on("queue:new_item", this.addQueueItem);
+    this.getInitialState()    
+  },
+  componentWillUnMount: function() {
+    window.app_dispatcher.off("queue:new_item", this.addQueueItem);
   },
   aMethod: function(){
       console.log('aMethod Called')  
@@ -32,16 +53,30 @@ var SingleSampleTree = React.createClass({
   formatParameters: function(paramType){
     var fields = [];
     //fields.push( <EditableField key={fieldno} sampleid={this.props.sample.sampleId} name={field} value={value} /> );
-      var paramDict = params[paramType]
+      var paramDict = params[paramType.split('_')[0]]
       for (var key in paramDict) {
           var value = paramDict[key]['default_value'];
           var name = paramDict[key]['label'];
-          console.log(key)
-          console.log(value)
           fields.push( <EditableField name={name} value={value} /> );
       }
-    console.log(fields)
     return fields
+  },
+  formatStatus: function(status){
+    switch (status){
+      case 0: return "fa fa-fw fa-circle-o";
+      case 1: return "fa fa-fw fa-check-circle-o";
+      case 2: return "fa fa-fw fa-exclamation-circle";
+    }
+  },
+  generateElementId: function(newElement){
+    var occurrences = 0
+    for (i = 0; i < this.state.list.length; i++){
+
+      if (this.state.list[i].name.split('_')[0]==newElement){
+          occurrences +=1
+      }  
+    }
+    return newElement+'_'+(occurrences+1)
   },
   render: function() {
     this.getInitialState()
@@ -49,7 +84,12 @@ var SingleSampleTree = React.createClass({
     var listStyle = {
       marginTop: '8px'
     };
+    console.log('rendering')
     var that = this
+    var arr = []
+    for (key in this.state.list){
+      arr.push(this.state.list[key])
+    }
     return (	
       <div className="panel panel-info">
           <div className="panel-heading">
@@ -60,13 +100,15 @@ var SingleSampleTree = React.createClass({
                   <b>Sample_42</b>
                   <ul className="lead list">
                   <ol className="text-left" >
-                          {this.state.list.map(function(listValue){
-                              return <li style={listStyle}> <a data-toggle="collapse" href={"#collapse"+listValue} > {listValue} </a><button type="button" className="btn btn-link  pull-right" onClick={that.aMethod}><i className="fa fa-fw fa-circle-o"></i></button>
-                                <button type="button" className="btn btn-link  pull-right" onClick={that.aMethod}><i className="fa fa-fw fa-play-circle"></i></button>
-                                <button type="button" className="btn btn-link  pull-right" onClick={that.aMethod}><i className="fa fa-fw fa-eraser"></i></button>  
-                                 <div className="collapse" id={"collapse"+listValue}>
+                          {arr.map(function(listValue){
+                              return <li style={listStyle}> <a data-toggle="collapse" href={"#collapse"+listValue['name']} > {listValue['name']} </a>
+                                <button type="button" className="btn btn-link  pull-right" onClick={that.aMethod}><i 
+                                  className={that.formatStatus(listValue['status'])}></i></button>
+                                <button type="button" className="btn btn-link  pull-right" onClick={that.runThisItem.bind(that,listValue['name'])}><i className="fa fa-fw fa-play-circle"></i></button>
+                                <button type="button" className="btn btn-link  pull-right" onClick={that.removeQueueItem.bind(that,listValue['name'])}><i className="fa fa-fw fa-eraser"></i></button>  
+                                 <div className="collapse" id={"collapse"+listValue['name']}>
                                     <div className="well">
-                                        {that.formatParameters(listValue)}
+                                        {that.formatParameters(listValue['name'])}
                                     </div>
                                   </div>
                                  </li>;
@@ -76,8 +118,8 @@ var SingleSampleTree = React.createClass({
               </div>
               <div className="col-md-2">
                     <hr></hr>
-                    <a className="btn btn-block btn-primary">Run  <i className="fa fa-play-circle fa-fw"></i></a>
-                    <a className="btn btn-block btn-primary">Stop  <i className="fa fa-stop fa-fw"></i></a>
+                    <a className="btn btn-block btn-primary" onClick={this.aMethod}>Run  <i className="fa fa-play-circle fa-fw"></i></a>
+                    <a className="btn btn-block btn-primary" onClick={this.aMethod}>Stop  <i className="fa fa-stop fa-fw"></i></a>
               </div>      
         </div>
       </div>
