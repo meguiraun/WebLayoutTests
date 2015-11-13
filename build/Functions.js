6ca5d160dad3a21d0be5b5f4aaf5766a5c7f7f05
Mikel Eguiraun <mikel.eguiraun@gmail.com>
2015-11-13 08:34:09 +0100

    doc added

--- build/Functions.js
+++
@@ -1,6 +0,0 @@
-/** @jsx React.DOM */
-
-console.log('dispatcher going to start');
-window.app_dispatcher = _.extend({}, Backbone.Events);
-console.log('dispatcher done');
-console.log(window.app_dispatcher)
